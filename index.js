b39b6f82fcae5764d388956789d9f94265aa998d
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-03-02 12:14:39 +0100

    Back pressure awareness

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ module.exports = function (pattern, options) {
 			return cb();
 		}
 
-		restoreStream.write(file);
-		cb();
+		restoreStream.write(file, cb);
 	});
 
 	stream.restore = function () {
